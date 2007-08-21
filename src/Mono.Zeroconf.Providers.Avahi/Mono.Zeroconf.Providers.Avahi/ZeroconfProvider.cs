8555e5d553d3bd2fc9729d05870c6c836012ff4c
Aaron Bockover <abockover@novell.com>
2007-08-21 18:28:44 +0000

    Updates
    svn path=/trunk/Mono.Zeroconf/; revision=84576

--- src/Mono.Zeroconf.Providers.Avahi/Mono.Zeroconf.Providers.Avahi/ZeroconfProvider.cs
+++ src/Mono.Zeroconf.Providers.Avahi/Mono.Zeroconf.Providers.Avahi/ZeroconfProvider.cs
@@ -4,7 +4,7 @@
 // Authors:
 //	Aaron Bockover  <abockover@novell.com>
 //
-// Copyright (C) 2006 Novell, Inc (http://www.novell.com)
+// Copyright (C) 2006-2007 Novell, Inc (http://www.novell.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -29,9 +29,9 @@
 using System;
 using System.Collections;
 
-[assembly:Mono.Zeroconf.ZeroconfProvider(typeof(Mono.Zeroconf.Avahi.ZeroconfProvider))]
+[assembly:Mono.Zeroconf.Providers.ZeroconfProvider(typeof(Mono.Zeroconf.Providers.Avahi.ZeroconfProvider))]
 
-namespace Mono.Zeroconf.Avahi
+namespace Mono.Zeroconf.Providers.Avahi
 {
     public class ZeroconfProvider : IZeroconfProvider
     {
