7cdd0eae783ba3a0e974f884fc877546bfe605b6
Aaron Bockover <abockover@novell.com>
2008-01-23 06:26:13 +0000

    2008-01-23  Aaron Bockover  <abockover@novell.com>
        0.7.4 Released
    
        * NEWS: Updated with 0.7.4 release notes
    
        * configure.ac: Output a zip file during dist for our Windows friends
    
        * Makefile.am: Rename the zip file containing the binaries for Windows
        so it doesn't conflict with the source zip file
    
        * COPYING: Updated copyright
    
    
    svn path=/trunk/Mono.Zeroconf/; revision=93620

--- src/Mono.Zeroconf.Providers.Bonjour/Mono.Zeroconf.Providers.Bonjour/BrowseService.cs
+++ src/Mono.Zeroconf.Providers.Bonjour/Mono.Zeroconf.Providers.Bonjour/BrowseService.cs
@@ -4,7 +4,7 @@
 // Authors:
 //    Aaron Bockover  <abockover@novell.com>
 //
-// Copyright (C) 2006-2007 Novell, Inc (http://www.novell.com)
+// Copyright (C) 2006-2008 Novell, Inc (http://www.novell.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
