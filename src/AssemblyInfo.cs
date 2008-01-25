3c99d0908a3fe4103f6bdc245391f57b6f007d75
Aaron Bockover <abockover@novell.com>
2008-01-25 00:33:08 +0000

    2008-01-24  Aaron Bockover  <abockover@novell.com>
        * configure.ac: Check for al, set policy/api version variables
    
        * src/Makefile.include: Rules for building the policy assembly and
        installing/uninstalling from the GAC; stolen from Mono.Addins
    
        * src/policy.config.in: Policy definition file
    
        * src/Mono.Zeroconf/Makefile.am: Build and install the assembly
        versioning policy
    
    
    svn path=/trunk/Mono.Zeroconf/; revision=93869

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly:AssemblyVersion("1.0.0.74")]
+[assembly:AssemblyVersion("1.0.0.75")]
 [assembly:AssemblyTitle("Mono.Zeroconf")]
 [assembly:AssemblyDescription("Cross Platform Zeroconf for .NET")]
 [assembly:AssemblyCopyright("Copyright (C) 2006-2008 Novell, Inc.")]
