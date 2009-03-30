6580a64464a109a4f5776957c796e926a73c3a8e
Aaron Bockover <abockover@novell.com>
2009-03-30 23:30:44 +0000

    Updated for 0.9.0
    svn path=/trunk/Mono.Zeroconf/; revision=130593

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly:AssemblyVersion("3.0.0.80")]
+[assembly:AssemblyVersion("4.0.0.90")]
 [assembly:AssemblyTitle("Mono.Zeroconf")]
 [assembly:AssemblyDescription("Cross Platform Zeroconf for .NET")]
-[assembly:AssemblyCopyright("Copyright (C) 2006-2008 Novell, Inc.")]
+[assembly:AssemblyCopyright("Copyright (C) 2006-2009 Novell, Inc.")]
 [assembly:AssemblyCompany("Novell, Inc.")]
 
