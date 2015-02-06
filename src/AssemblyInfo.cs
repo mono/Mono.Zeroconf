4859fe5dd33d955762ae652a618487860d26a114
unknown <mark@W233.local.avolites.com>
2015-02-06 12:29:50 +0000

    Update version number for bug fix

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly:AssemblyVersion("4.0.0.90")]
+[assembly:AssemblyVersion("4.0.1.90")]
 [assembly:AssemblyTitle("Mono.Zeroconf")]
 [assembly:AssemblyDescription("Cross Platform Zeroconf for .NET")]
 [assembly:AssemblyCopyright("Copyright (C) 2006-2009 Novell, Inc.")]
